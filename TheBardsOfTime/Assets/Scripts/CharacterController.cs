844cc1c5c5277dd75989bf3d3ed54e6038f2f446
Eonii <tasplund90@gmail.com>
2016-11-14 20:39:23 +0200

    Improved movement
    + double jump

--- TheBardsOfTime/Assets/Scripts/CharacterController.cs
+++ TheBardsOfTime/Assets/Scripts/CharacterController.cs
@@ -3,13 +3,40 @@ using System.Collections;
 
 public class CharacterController : MonoBehaviour {
 
-    public float inputDelay = 0.1f;
-    public float forwardVel = 12;
-    public float rotateVel = 100;
 
+    public bool canDoubleJump = false;
+    [System.Serializable]
+    public class MoveSettings {
+        public float forwardVel = 12;
+        public float rotateVel = 100;
+        public float jumpVel = 25;
+        public float doubleJumpVel = 50;
+        public float distToGrounded = 1.6f;
+        public LayerMask ground;
+
+    } 
+
+    [System.Serializable]
+    public class PhysSettings {
+        public float downAccel = 0.75f;
+    } 
+
+    [System.Serializable]
+    public class InputSettings {
+        public float inputDelay = 0.1f;
+        public string FORWARD_AXIS = "Vertical";
+        public string TURN_AXIS = "Horizontal";
+        public string JUMP_AXIS = "Jump";
+    }
+
+    public MoveSettings moveSetting = new MoveSettings();
+    public PhysSettings physSetting = new PhysSettings();
+    public InputSettings inputSetting = new InputSettings();
+
+    Vector3 velocity = Vector3.zero;
     Quaternion targetRotation;
     Rigidbody rb;
-    float forwardInput, turnInput;
+    float forwardInput, turnInput, jumpInput;
 
     public Collider[] attackHitBoxes;
     private string Instrument = "flute";
@@ -19,47 +46,55 @@ public class CharacterController : MonoBehaviour {
         get { return targetRotation; }
     }
 
+    bool Grounded() {
+        return Physics.Raycast(transform.position, Vector3.down, moveSetting.distToGrounded, moveSetting.ground);
+    }
 
     void Start() {
         targetRotation = transform.rotation;
         rb = GetComponent<Rigidbody>();
 
-        forwardInput = turnInput = 0;
+        forwardInput = turnInput = jumpInput = 0;
     }
 
     void GetInput() {
-        forwardInput = Input.GetAxis("Vertical");
-        turnInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis(inputSetting.FORWARD_AXIS);
+        turnInput = Input.GetAxis(inputSetting.TURN_AXIS);
+        jumpInput = Input.GetAxisRaw(inputSetting.JUMP_AXIS);
     }
 
 
     void Update() {
         GetInput();
         Turn();
+        Debug.DrawRay(transform.position, Vector3.down, Color.red);
     }
 
     void FixedUpdate() {
         Run();
+        Jump();
+        DoubleJump();
+        rb.velocity = transform.TransformDirection(velocity);
         if (Input.GetKeyDown(KeyCode.F))
             //Attack(attackHitBoxes[0]);
             Shoot(Instrument);
     }
 
     void Run() {
-        if(Mathf.Abs(forwardInput) > inputDelay) {
+        if(Mathf.Abs(forwardInput) > inputSetting.inputDelay) {
             //move
-            rb.velocity = transform.forward * forwardInput * forwardVel;
+            velocity.z = moveSetting.forwardVel * forwardInput;
 
         }else {
             //no velocity
-            rb.velocity = Vector3.zero;
+            velocity.z = 0;
         }
     }
 
 
     void Turn() {
-        if (Mathf.Abs(turnInput) > inputDelay) {
-            targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
+        if (Mathf.Abs(turnInput) > inputSetting.inputDelay) {
+            targetRotation *= Quaternion.AngleAxis(moveSetting.rotateVel * turnInput * Time.deltaTime, Vector3.up);
         }
         transform.rotation = targetRotation;
     }
@@ -87,6 +122,26 @@ public class CharacterController : MonoBehaviour {
                     break;
                 }
             default: break;
+        }
+    }
+
+    void DoubleJump() {
+        if(Input.GetKeyDown(KeyCode.Space) && canDoubleJump) {
+            //velocity.y = moveSetting.jumpVel;
+            velocity.y = moveSetting.doubleJumpVel;
+            canDoubleJump = false;
+        }
+    }
+    void Jump() {
+        if(jumpInput > 0 && Grounded()) {
+            velocity.y = moveSetting.jumpVel;
+            canDoubleJump = true;
+        }
+        else if(jumpInput == 0 && Grounded()) {
+            velocity.y = 0;
+        }
+        else {
+            velocity.y -= physSetting.downAccel;
         }
     }
 
