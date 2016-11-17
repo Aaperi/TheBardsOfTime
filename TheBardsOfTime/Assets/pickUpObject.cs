8200223a64d2246f4770c8135e6a5bb63e2990b9
Eonii <tasplund90@gmail.com>
2016-11-17 18:56:24 +0200

    Pickup tehty pelaajalle
    tavaran nostamis nappi on bindattu E kirjaimeen,
    pelaajan täytyy olla naama tavaraa vasten että voi nostaa

---
+++ TheBardsOfTime/Assets/pickUpObject.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickUpObject : MonoBehaviour {
+
+    bool carrying;
+    GameObject carriedObject;
+    public float smooth;
+    bool carriedObjectExists = false;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E) && carriedObjectExists) {
+            if (!carrying) {
+                carrying = true;
+                carriedObject.GetComponent<Rigidbody>().isKinematic = true;
+            } else {
+                dropObject();
+            }
+        }
+ 
+        if (carrying) {
+            carry(carriedObject);
+        }
+	}
+
+    void carry(GameObject o) {
+        if(carriedObjectExists)
+        o.transform.position = Vector3.Lerp(o.transform.position, GameObject.Find("pickupPlace").transform.position, Time.deltaTime * smooth );
+    }
+
+
+    void OnTriggerEnter(Collider col) {
+        
+        if(col.gameObject.tag == "pickup") {
+            carriedObjectExists = true;
+        carriedObject = col.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider col) {
+        if(col.gameObject == carriedObject && !carrying) {
+            carriedObject = null;
+            carriedObjectExists = false;
+        }
+    }
+
+
+    void dropObject() {
+        carrying = false;
+        carriedObject.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        carriedObject = null;
+    }
+
+}
+
