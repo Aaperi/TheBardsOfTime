d5f686348d029fa315db53b59c0de0cfcb6974f4
Eonii <tasplund90@gmail.com>
2016-12-20 14:19:34 +0200

    kamera paranneltu

--- TheBardsOfTime/Assets/Scripts/CharacterController.cs
+++ TheBardsOfTime/Assets/Scripts/CharacterController.cs
@@ -1,28 +1,29 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class CharacterController : MonoBehaviour {
 
-
-    public bool canDoubleJump = false;
     [System.Serializable]
-    public class MoveSettings {
+    public class MoveSettings
+    {
         public float forwardVel = 12;
         public float rotateVel = 100;
         public float jumpVel = 25;
-        public float doubleJumpVel = 50;
-        public float distToGrounded = 1.6f;
+        public float distToGrounded = 0.1f;
         public LayerMask ground;
-
-    } 
+    }
 
     [System.Serializable]
-    public class PhysSettings {
+    public class PhysSettings
+    {
         public float downAccel = 0.75f;
-    } 
+    }
 
     [System.Serializable]
-    public class InputSettings {
+    public class InputSettings
+    {
         public float inputDelay = 0.1f;
         public string FORWARD_AXIS = "Vertical";
         public string TURN_AXIS = "Horizontal";
@@ -35,86 +36,79 @@ public class CharacterController : MonoBehaviour {
 
     Vector3 velocity = Vector3.zero;
     Quaternion targetRotation;
-    Rigidbody rb;
+    Rigidbody rBody;
     float forwardInput, turnInput, jumpInput;
-
-
 
     public Quaternion TargetRotation
     {
         get { return targetRotation; }
     }
 
-    bool Grounded() {
-        return Physics.Raycast(transform.position, Vector3.down, moveSetting.distToGrounded, moveSetting.ground);
+    bool Grounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 
+            moveSetting.distToGrounded, moveSetting.ground);
     }
 
-    void Start() {
+	// Use this for initialization
+	void Start () {
         targetRotation = transform.rotation;
-        rb = GetComponent<Rigidbody>();
-
+        rBody = GetComponent<Rigidbody>();
         forwardInput = turnInput = jumpInput = 0;
-    }
+	}
 
-    void GetInput() {
+    void GetInput()
+    {
         forwardInput = Input.GetAxis(inputSetting.FORWARD_AXIS);
         turnInput = Input.GetAxis(inputSetting.TURN_AXIS);
         jumpInput = Input.GetAxisRaw(inputSetting.JUMP_AXIS);
     }
-
-
-    void Update() {
+	
+	// Update is called once per frame
+	void Update () {
         GetInput();
-        Turn();
+        Turn();		
+	}
 
-        Debug.DrawRay(transform.position, Vector3.down, Color.red);
-    }
-
-    void FixedUpdate() {
+    private void FixedUpdate()
+    {
         Run();
         Jump();
-        DoubleJump();
-        rb.velocity = transform.TransformDirection(velocity);
+
+        rBody.velocity = transform.TransformDirection(velocity);
     }
 
-    void Run() {
-        if(Mathf.Abs(forwardInput) > inputSetting.inputDelay) {
-            //move
+    void Run()
+    {
+        if (Mathf.Abs(forwardInput) > inputSetting.inputDelay) {
             velocity.z = moveSetting.forwardVel * forwardInput;
-
-        }else {
-            //no velocity
+        } else {
             velocity.z = 0;
         }
     }
 
-
-    void Turn() {
-        if (Mathf.Abs(turnInput) > inputSetting.inputDelay) {
+    void Turn()
+    {
+        if (Mathf.Abs(forwardInput) > inputSetting.inputDelay)
+        {
             targetRotation *= Quaternion.AngleAxis(moveSetting.rotateVel * turnInput * Time.deltaTime, Vector3.up);
         }
+
         transform.rotation = targetRotation;
     }
 
-    void DoubleJump() {
-        if(Input.GetKeyDown(KeyCode.Space) && canDoubleJump) {
-            //velocity.y = moveSetting.jumpVel;
-            velocity.y = moveSetting.doubleJumpVel;
-            canDoubleJump = false;
-        }
-    }
-    void Jump() {
-        if(jumpInput > 0 && Grounded()) {
+    void Jump()
+    {
+        if (jumpInput > 0 && Grounded()) {
+            // On ground, pressing jump 
             velocity.y = moveSetting.jumpVel;
-            canDoubleJump = true;
-        }
-        else if(jumpInput == 0 && Grounded()) {
+            Debug.Log("JUMP");
+        } else if (jumpInput == 0 && Grounded()) {
+            // On ground, not pressing jump
             velocity.y = 0;
-        }
-        else {
+        } else {
+            // On air
             velocity.y -= physSetting.downAccel;
         }
     }
-
-
 }
