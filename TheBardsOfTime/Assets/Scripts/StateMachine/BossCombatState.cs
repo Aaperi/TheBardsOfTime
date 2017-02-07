93414a60df7ef3abd8e649a57b9443d75bb48590
Ari-Pekka Åttman <apattman@gmail.com>
2017-02-07 11:30:28 +0200

    BossScriptejä

---
+++ TheBardsOfTime/Assets/Scripts/StateMachine/BossCombatState.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossCombatState : IBossState {
+
+    private readonly StatePatternBoss boss;
+    private HPScript hp = GameObject.FindGameObjectWithTag("Player").GetComponent<HPScript>();
+    private float attackCoolDown = .75f;
+    private Transform player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    public BossCombatState(StatePatternBoss statePatternBoss) {
+        boss = statePatternBoss;
+    }
+
+    public void UpdateState() {
+
+    }
+
+    public void OnTriggerEnter(Collider other) {
+
+    }
+
+    public void OnTriggerExit(Collider other) {
+
+    }
+
+    public void ToCastingState() {
+
+    }
+
+    public void ToCombatState() {
+
+    }
+
+    void Attack() {
+        attackCoolDown -= Time.deltaTime;
+        RaycastHit hit;
+        if (Physics.Raycast(boss.transform.position, boss.transform.forward, out hit, boss.sightRange, boss.mask) && hit.collider.CompareTag("Player")) {
+            if (attackCoolDown <= 0) {
+                hp.TakeDamage(10f);
+                attackCoolDown = .75f;
+            }
+        }
+    }
+}
