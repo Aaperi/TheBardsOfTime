680a283ad108cf6f34fdb47f7a28cabbd175259a
Kolppan3n <kolppan3n@gmail.com>
2017-03-23 22:46:19 +0200

    Palikanpaluu

--- TheBardsOfTime/Assets/Scripts/Puzzles/Pickupable.cs
+++ TheBardsOfTime/Assets/Scripts/Puzzles/Pickupable.cs
@@ -1,8 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pickupable : MonoBehaviour {
+public class Pickupable : MonoBehaviour
+{
 
     public int weight;
+    Vector3 Opos;
+
+    void Start()
+    {
+        Opos = transform.position;
+    }
+
+    void Update()
+    {
+        if (transform.position.y < -50) {
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            transform.position = Opos;
+        }
+    }
 
 }
