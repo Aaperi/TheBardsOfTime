3f8a92e3741d082deeff067173dd86f468d39bb6
Kolppan3n <kolppan3n@gmail.com>
2017-03-27 23:53:05 +0300

    leijuva itemi jonka voi napata

---
+++ TheBardsOfTime/Assets/ItemCode.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemCode : MonoBehaviour
+{
+
+    Quaternion rot;
+    Vector3 Opos;
+    Vector3 velocity = Vector3.zero;
+    bool goingUP = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        Opos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 1.5f, 0));
+
+        if (goingUP) {
+            transform.position = new Vector3(Opos.x, transform.position.y + 0.02f, Opos.z);
+            if (transform.position.y > Opos.y + 1)
+                goingUP = false;
+        } else {
+            transform.position = new Vector3(Opos.x, transform.position.y - 0.02f, Opos.z);
+            if (transform.position.y < Opos.y)
+                goingUP = true;
+        }
+    }
+}
---
+++ TheBardsOfTime/Assets/ItemPicker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPicker : MonoBehaviour {
+
+    int notes = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag.Equals("Item") && col.gameObject != null) {
+            notes++;
+            Debug.Log("You got " + notes + " notes");
+        }
+        Destroy(col.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
