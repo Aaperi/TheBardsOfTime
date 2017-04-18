ffaf50df23e7ef0136d162953462ad76ee31cb5a
Kolppan3n <Kolppan3n@gmail.com>
2017-04-18 13:08:11 +0300

    paskavittua

--- TheBardsOfTime/Assets/Scripts/CC.cs
+++ TheBardsOfTime/Assets/Scripts/CC.cs
@@ -231,6 +231,8 @@ public class CC : MonoBehaviour
     void MoveToStart()
     {
         gm = FindObjectOfType<GameManager>();
+        Debug.Log(gm.lastLevelID + " LLID");
+        Debug.Log(gm.lastPos.Length + " Putkuus");
         if (gm.lastPos.Length > 0) {
             transform.position = new Vector3(gm.lastPos[0], gm.lastPos[1], gm.lastPos[2]);
             transform.eulerAngles = new Vector3(0, gm.lastPos[3], 0);
